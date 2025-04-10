6a3061f4ae26b60ce1cf11adc62263f09c94abb1
HTKyuu <79772156+HTKyuu@users.noreply.github.com>
2025-04-10 13:24:40 +0200

    Update bladeball.lua

--- bladeball.lua
+++ bladeball.lua
@@ -16,7 +16,9 @@ local realBall = nil
 local lastPosition = nil
 local lastTime = nil
 
-local hasPressedF = false 
+local Connection = nil
+local hasPressedF = false
+local Cooldown = tick()
 local autoBall = true
 local teleport = true
 local autoTrainingBall = true
@@ -217,7 +219,8 @@ RunService.RenderStepped:Connect(function(dt)
                     VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.F, false, game) -- F key press
                     VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.F, false, game) -- F key release
                     -- print(string.format("Success distance: %.2f studs, Speed Towards Player: %.2f studs/sec", distanceToPlayer, speedTowardsPlayer))
-                    hasPressedF = true  
+                    hasPressedF = true
+                    Cooldown = tick()
                 end
             end
 
@@ -225,10 +228,10 @@ RunService.RenderStepped:Connect(function(dt)
             lastPosition = currentPosition
             lastTime = currentTime
         -- check if targetting different player
-        else
-            if realBall and realBall:GetAttribute("target") ~= plr.Name then
-                hasPressedF = false
-            end
+     --   else
+          --  if realBall and realBall:GetAttribute("target") ~= plr.Name then
+             --   hasPressedF = false
+           -- end
             lastPosition = nil
             lastTime = nil
         end
@@ -310,7 +313,8 @@ RunService.RenderStepped:Connect(function(dt)
                     VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.F, false, game) -- F key press
                     VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.F, false, game) -- F key release
                     -- print(string.format("Success distance: %.2f studs, Speed Towards Player: %.2f studs/sec", distanceToPlayer, speedTowardsPlayer))
-                    hasPressedF = true  
+                    hasPressedF = true 
+                    Cooldown = tick()
                 end
             end
 
@@ -318,10 +322,10 @@ RunService.RenderStepped:Connect(function(dt)
             lastPosition = currentPosition
             lastTime = currentTime
         -- check if targetting different player
-        else
-            if realBall and realBall:GetAttribute("target") ~= plr.Name then
-                hasPressedF = false
-            end
+       -- else
+         --   if realBall and realBall:GetAttribute("target") ~= plr.Name then
+           --     hasPressedF = false
+          --  end
             lastPosition = nil
             lastTime = nil
         end
